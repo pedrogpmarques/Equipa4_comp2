168766f5a726857611dcb79240edb66437ad14fb
marques <marques@marques-Lenovo-V130-14IKB>
2025-11-21 11:57:51 +0000

    initial commit w springboot based project

---
+++ gestaodeeventos/src/test/java/com/upt/lp/gestaodeeventos/GestaoDeEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upt.lp.gestaodeeventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
