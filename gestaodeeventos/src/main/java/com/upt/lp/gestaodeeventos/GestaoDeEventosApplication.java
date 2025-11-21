96614064df1a2360a4704aa7b6e356dce98b4387
marques <marques@marques-Lenovo-V130-14IKB>
2025-11-21 11:49:57 +0000

    Initial commit with springboot based project

---
+++ gestaodeeventos/src/main/java/com/upt/lp/gestaodeeventos/GestaoDeEventosApplication.java
@@ -0,0 +1,13 @@
+package com.upt.lp.gestaodeeventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeEventosApplication.class, args);
+	}
+
+}
