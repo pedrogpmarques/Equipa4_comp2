b1ade22f4a3befd586bba1cec09e73db4f0c3be0
diogo <diogo@LAPTOP-ID4NJEII>
2025-12-16 12:36:29 +0000

    atualizacao

--- gestaodeeventos/src/main/java/com/upt/lp/gestaodeeventos/dto/InscricaoDTO.java
+++ gestaodeeventos/src/main/java/com/upt/lp/gestaodeeventos/dto/InscricaoDTO.java
@@ -7,59 +7,55 @@ import java.time.LocalDateTime;
 public class InscricaoDTO {
 
     private Integer id;
+
+    // mantém IDs (úteis para regras e operações)
     private Integer eventoId;
     private Integer participanteId;
+
+    // novos campos para UI
+    private String eventoTitulo;
+    private String participanteNome;
+
     private LocalDateTime dataInscricao;
     private String estadoInscricao;
 
-    public InscricaoDTO() {
-    }
+    public InscricaoDTO() {}
 
     public InscricaoDTO(Inscricao i) {
         this.id = i.getId();
-        this.eventoId = i.getEvento() != null ? i.getEvento().getId() : null;
-        this.participanteId = i.getParticipante() != null ? i.getParticipante().getId() : null;
+
+        if (i.getEvento() != null) {
+            this.eventoId = i.getEvento().getId();
+            this.eventoTitulo = i.getEvento().getTitulo(); // <---
+        }
+
+        if (i.getParticipante() != null) {
+            this.participanteId = i.getParticipante().getId();
+            this.participanteNome = i.getParticipante().getNomeUtilizador(); // <---
+        }
+
         this.dataInscricao = i.getDataInscricao();
-        this.estadoInscricao = i.getEstadoInscricao() != null ? i.getEstadoInscricao().name() : null;
+        this.estadoInscricao = (i.getEstadoInscricao() != null) ? i.getEstadoInscricao().name() : null;
     }
 
-    public Integer getId() {
-        return id;
-    }
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+    public Integer getEventoId() { return eventoId; }
+    public void setEventoId(Integer eventoId) { this.eventoId = eventoId; }
 
-    public Integer getEventoId() {
-        return eventoId;
-    }
+    public Integer getParticipanteId() { return participanteId; }
+    public void setParticipanteId(Integer participanteId) { this.participanteId = participanteId; }
 
-    public void setEventoId(Integer eventoId) {
-        this.eventoId = eventoId;
-    }
+    public String getEventoTitulo() { return eventoTitulo; }
+    public void setEventoTitulo(String eventoTitulo) { this.eventoTitulo = eventoTitulo; }
 
-    public Integer getParticipanteId() {
-        return participanteId;
-    }
+    public String getParticipanteNome() { return participanteNome; }
+    public void setParticipanteNome(String participanteNome) { this.participanteNome = participanteNome; }
 
-    public void setParticipanteId(Integer participanteId) {
-        this.participanteId = participanteId;
-    }
+    public LocalDateTime getDataInscricao() { return dataInscricao; }
+    public void setDataInscricao(LocalDateTime dataInscricao) { this.dataInscricao = dataInscricao; }
 
-    public LocalDateTime getDataInscricao() {
-        return dataInscricao;
-    }
-
-    public void setDataInscricao(LocalDateTime dataInscricao) {
-        this.dataInscricao = dataInscricao;
-    }
-
-    public String getEstadoInscricao() {
-        return estadoInscricao;
-    }
-
-    public void setEstadoInscricao(String estadoInscricao) {
-        this.estadoInscricao = estadoInscricao;
-    }
+    public String getEstadoInscricao() { return estadoInscricao; }
+    public void setEstadoInscricao(String estadoInscricao) { this.estadoInscricao = estadoInscricao; }
 }
